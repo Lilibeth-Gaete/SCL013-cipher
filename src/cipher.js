3fdb48dab414926d3ff43029e2260f7639662b19
Lilibeth-Gaete <lilibeth.gaete@gmail.com>
2020-04-08 17:14:47 -0400

    cipher.js e index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,46 @@
-const cipher = {
-  // ...
-};
+const cipher= {
 
-export default cipher;
+  encode:(n,escrito)=>{
+  let texto = "";
+  let termino="";
+  let i;
+  
+    for (i = 0; i < escrito.length; i++) {
+       
+    if (escrito.charCodeAt(i)>=65 && escrito.charCodeAt(i)<=90){
+     
+          texto= (((escrito.charCodeAt(i)-65)+parseInt(n))%26)+65;
+        
+         }
+      else if(escrito.charCodeAt(i)===32){
+          texto= 32;
+          }
+          termino += String.fromCharCode(texto);
+   
+   }
+  return termino;
+  },
+  decode:(n,escrito)=>{
+  let texto = "";
+  let termino="";
+  let i;
+  
+    for (i = 0; i < escrito.length; i++) {
+       
+    if (escrito.charCodeAt(i)>=65 && escrito.charCodeAt(i)<=90){
+     
+          texto= (((escrito.charCodeAt(i)-65)-parseInt(n))%26)+65;
+        
+         }
+      else if(escrito.charCodeAt(i)===32){
+          texto= 32;
+          }
+          termino += String.fromCharCode(texto);
+  
+   }
+   return termino;
+  }
+    };
+  
+  export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,61 +1,26 @@
-//import cipher from './cipher.js';
+import cipher from './cipher.js';
 
-//console.log(cipher);
-
-
-function nombreAgente() {
-  var nombre = document.getElementById("name-example").value;
-  document.getElementById("comienzo").innerHTML = "Detective " + nombre + " empieza tu misión";
-
+document.getElementById("juego").addEventListener("click",nombreAgente) ;
+function nombreAgente(){
+  var nombre= document.getElementById("name-example").value;
+document.getElementById("comienzo").innerHTML="Detective "+nombre+ " empieza tu misión"; 
 }
 
-function textoNormal() {
-  var escrito = document.getElementById("normal").value;
-  var n = document.getElementById("numero").value;
-  var texto = "";
-  var termino = "";
-  var i;
-  for (i = 0; i < escrito.length; i++) {
-    if (escrito.charCodeAt(i) >= 65 && escrito.charCodeAt(i) <= 90) {
-      texto = (((escrito.charCodeAt(i) - 65) + parseInt(n)) % 26) + 65;
-    }
-    else if (escrito.charCodeAt(i) === 32) {
-      texto = 32;
-    }
-    termino += String.fromCharCode(texto);
-    document.getElementById("secreto").innerHTML = termino;
-  }
+document.getElementById("botonCifrar").addEventListener("click",textoNormal);
+
+function textoNormal(){
+  let n =document.getElementById("numero").value;
+  let escrito = document.getElementById("normal").value;
+  //let termino=document.getElementById("secreto").innerHTML = termino;
+  cipher.encode(n,escrito);
 }
 
 
-function textoEncriptado() {
-  var escrito = document.getElementById("secreto").value;
-  var n = document.getElementById("numero").value;
-  var texto = "";
-  var termino = "";
-  var i;
-  for (i = 0; i < escrito.length; i++) {
-    if (escrito.charCodeAt(i) >= 65 && escrito.charCodeAt(i) <= 90) {
-      texto = (((escrito.charCodeAt(i) - 65) + parseInt(n)) % 26) + 65;
-    }
-    else if (escrito.charCodeAt(i) === 32) {
-      texto = 32;
-    }
-    termino += String.fromCharCode(texto);
-    document.getElementById("normal").innerHTML = termino;
-  }
+document.getElementById("botonDescifrar").addEventListener("click",textoEncriptado);
+
+function textoEncriptado(){
+  let n =document.getElementById("numero").value;
+  let escrito = document.getElementById("normal").value;
+  //let termino=document.getElementById("secreto").innerHTML = termino;
+  cipher.decode(n,escrito);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
