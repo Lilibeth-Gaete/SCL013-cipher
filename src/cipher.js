d65aa1f669a7a7dc8ac66b0fb0515e9bd302eca4
fer <amontecinoscontreras@gmail.com>
2020-04-08 17:42:28 -0400

    cambios en cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,45 @@
 const cipher = {
-  // ...
+
+  encode: (n, escrito) => {
+    let texto = "";
+    let termino = "";
+    let i;
+
+    for (i = 0; i < escrito.length; i++) {
+
+      if (escrito.charCodeAt(i) >= 65 && escrito.charCodeAt(i) <= 90) {
+
+        texto = (((escrito.charCodeAt(i) - 65) + parseInt(n)) % 26) + 65;
+
+      }
+      else if (escrito.charCodeAt(i) === 32) {
+        texto = 32;
+      }
+      termino += String.fromCharCode(texto);
+
+    }
+    return termino;
+  },
+  decode: (n, escrito) => {
+    let texto = "";
+    let termino = "";
+    let i;
+
+    for (i = 0; i < escrito.length; i++) {
+
+      if (escrito.charCodeAt(i) >= 65 && escrito.charCodeAt(i) <= 90) {
+
+        texto = (((escrito.charCodeAt(i) - 65) - parseInt(n)) % 26) + 65;
+
+      }
+      else if (escrito.charCodeAt(i) === 32) {
+        texto = 32;
+      }
+      termino += String.fromCharCode(texto);
+
+    }
+    return termino;
+  }
 };
 
 export default cipher;
