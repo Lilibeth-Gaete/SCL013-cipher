fb23ca57695afc8511ba120022b97760464964b9
fer <amontecinoscontreras@gmail.com>
2020-04-13 09:22:08 -0400

    cambios cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const cifrado= {
   encode :function (n,escrito){
   let texto = "";
@@ -40,5 +41,49 @@ termino += String.fromCharCode(texto);
 }
  }
     export default cifrado; 
+=======
 
 
+const cifrado = {
+  encode: function (n, escrito) {
+    let texto = "";
+    let termino = "";
+    let i;
+
+    for (i = 0; i < escrito.length; i++) {
+
+      if (escrito.charCodeAt(i) >= 65 && escrito.charCodeAt(i) <= 90) {
+
+        texto = (((escrito.charCodeAt(i) - 65) + parseInt(n)) % 26) + 65;
+
+      }
+      else if (escrito.charCodeAt(i) === 32) {
+        texto = 32;
+      }
+
+      termino += String.fromCharCode(texto);
+    }
+    return termino;
+  },
+  decode: function (n, escrito) {
+    let texto = "";
+    let termino = "";
+    let i;
+
+    for (i = 0; i < escrito.length; i++) {
+>>>>>>> cambios cipher
+
+      if (escrito.charCodeAt(i) >= 65 && escrito.charCodeAt(i) <= 90) {
+
+        texto = (((escrito.charCodeAt(i) - 65) - parseInt(n)) % 26) + 65;
+
+      }
+      else if (escrito.charCodeAt(i) === 32) {
+        texto = 32;
+      }
+      termino += String.fromCharCode(texto);
+    }
+    return termino;
+  }
+}
+export default cifrado; 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import cifrado from './cipher.js';
 
 
-document.getElementById("juego").addEventListener("click",nombreAgente) ;
-function nombreAgente(){
-  var nombre= document.getElementById("name-example").value;
-document.getElementById("comienzo").innerHTML="Detective "+nombre+ " empieza tu misión"; 
+document.getElementById("juego").addEventListener("click", nombreAgente);
+function nombreAgente() {
+  var nombre = document.getElementById("name-example").value;
+  document.getElementById("comienzo").innerHTML = "Detective " + nombre + " empieza tu misión";
 }
 
-document.getElementById("botonCifrar").addEventListener("click",textoNormal);
+document.getElementById("botonCifrar").addEventListener("click", textoNormal);
 
+<<<<<<< HEAD
 function textoNormal(){
   let termino="";
   let n =document.getElementById("numero").value;
@@ -25,3 +26,21 @@ function textoEncriptado(){
  cifrado.decode(n,escrito);
   let final=document.getElementById("normal").innerHTML = cifrado.decode(n,escrito);
   }
+=======
+function textoNormal() {
+  var final = "";
+  let n = document.getElementById("numero").value;
+  let escrito = document.getElementById("normal").value;
+  cifrado.encode(n, escrito);
+  final = document.getElementById("secreto").innerHTML = cifrado.encode(n, escrito);
+}
+
+document.getElementById("botonDescifrar").addEventListener("click", textoEncriptado);
+
+function textoEncriptado() {
+  let n = document.getElementById("numero").value;
+  let escrito = document.getElementById("secreto").value;
+  cifrado.decode(n, escrito);
+  var final = document.getElementById("normal").innerHTML = cifrado.decode(n, escrito);
+}
+>>>>>>> cambios cipher
